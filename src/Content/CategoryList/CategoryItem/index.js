2f4abbaf7d630928f8b5d62bfa6fa95f8558b906
fabulae <abhimediratta28@gmail.com>
2018-04-14 23:49:05 +0530

    Icon alignment set

--- src/Content/CategoryList/CategoryItem/index.js
+++ src/Content/CategoryList/CategoryItem/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Flex } from 'grid-styled';
+import { Flex, Box } from 'grid-styled';
 import styled from 'styled-components';
 
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
@@ -7,14 +7,22 @@ import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 const ItemBox = styled(Flex)`
   background: ${props => props.theme.colors.categoryItem.background};
   color: ${props => props.theme.colors.categoryItem.color};
+  flex: 1;
+  min-width: 0;
+`;
+
+const ItemBoxIcon = styled(Box)`
+  font-size: 4em;
 `;
 
 export default class CategoryItem extends Component {
   render () {
     var { category } = this.props;
     return (
-      <ItemBox mr={10} px={10} flexDirection='column' justify='center' alignItems='center'>
-        <FontAwesomeIcon icon="burn"/>
+      <ItemBox mr={10} px={30} py={40} flexDirection='column' justify='center' alignItems='center'>
+        <ItemBoxIcon mb={10}>
+          <FontAwesomeIcon icon="burn"/>
+        </ItemBoxIcon>
         {category}
       </ItemBox>
     )
